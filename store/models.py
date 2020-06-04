03ff593154d967553fce18abe183576b70be0f4e
Henry <hgonzalez96@outlook.com>
2020-06-04 15:04:50 -0400

    comments model creation

--- store/models.py
+++ store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 
 # Create your models here.
@@ -7,5 +9,12 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     genre = models.CharField(max_length=25)
-    publication_date = models.DateTimeField(default=now)
+    pub_date = models.DateTimeField(default=now())
     description = models.TextField(max_length=200)
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    comment = HTMLField('Comment')
+    pub_date = models.DateTimeField(default=now())
