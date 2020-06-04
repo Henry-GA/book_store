bdf4a4815eb867891651365a2963ba23ee0f63d0
Henry <hgonzalez96@outlook.com>
2020-06-04 15:25:43 -0400

    add images to the book model and create a new migration

--- store/models.py
+++ store/models.py
@@ -9,7 +9,8 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     genre = models.CharField(max_length=25)
-    pub_date = models.DateTimeField(default=now())
+    image = models.ImageField(upload_to='static/web/media/')
+    pub_date = models.DateTimeField(default=now)
     description = models.TextField(max_length=200)
 
 
@@ -17,4 +18,4 @@ class Comments(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     comment = HTMLField('Comment')
-    pub_date = models.DateTimeField(default=now())
+    pub_date = models.DateTimeField(default=now)
