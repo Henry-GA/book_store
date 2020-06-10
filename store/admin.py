60e001bb5484b4d571e37be761101c034c57de9e
Henry <hgonzalez96@outlook.com>
2020-06-10 15:29:17 -0400

    Modify the default django-admin view

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from store.models import Book, Comments
 
+
 # Register your models here.
-admin.site.register(Book)
+class BooksAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'genre', 'pub_date')
+    list_filter = ['pub_date']
+
+
+admin.site.register(Book, BooksAdmin)
 admin.site.register(Comments)
