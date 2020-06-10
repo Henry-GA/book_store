c75cd07fe6fcbadfc6f5f17871bf1bb1c6693f4a
Henry <hgonzalez96@outlook.com>
2020-06-10 15:34:55 -0400

    create detail and detail views

--- store/views.py
+++ store/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+
 
 # Create your views here.
+
+def index(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'web/index.html', context)
+
+
+def detail(request, id):
+    book = get_object_or_404(Book, pk=id)
+    context = {'book': book}
+    return render(request, 'web/detail.html', context)
