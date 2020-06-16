cd8e2d1e7f4ce98a06a1024a135f946cb23fc810
Henry <hgonzalez96@outlook.com>
2020-06-16 12:02:50 -0400

    Update the book model

--- store/models.py
+++ store/models.py
@@ -9,9 +9,11 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     genre = models.CharField(max_length=25)
+    pages = models.IntegerField(default=0)
     image = models.ImageField(upload_to='static/web/media/')
     pub_date = models.DateTimeField(default=now)
     description = models.TextField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
 
 
 class Comments(models.Model):
